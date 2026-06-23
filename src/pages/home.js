c3723a9a8d729f50be0f8ad7a58d59cbc93d7e37
LeejanAlipio <alipioml0131@gmail.com>
2026-06-23 17:00:48 +0800

    feat: Finish home page function

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,2 +1,20 @@
 import "../styles/home.css";
-import cafe from "../assets/cafe.jpg"
+
+export default function createHomePage() {
+    const container = document.querySelector('#content');
+    container.classList.add('content');
+
+    const heroSection = document.createElement('div');
+    heroSection.classList.add('hero-section');
+    heroSection.innerHTML = `
+        <p class="hero-year">ESTABLISHED 2024</p>
+        <h1 class="hero-title">Honest Ingredients. Crafted with Heart.</h1>
+        <p class="hero-description">Experience the raw beauty of seasonal dining. We source every ingredient from local family farms to bring the purest flavors of the earth directly to your table.</p>
+        <div class="hero-btn-container">
+            <button class="menu-btn">EXPLORE MENU</button>
+            <button class="about-btn">OUR STORY</button>
+        </div>
+        `
+
+    container.appendChild(heroSection);
+}
