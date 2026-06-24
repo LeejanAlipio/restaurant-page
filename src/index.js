107d1beaff7e9c77bda241887b61bd8d0c074d8a
LeejanAlipio <alipioml0131@gmail.com>
2026-06-24 11:09:10 +0800

    feat: Add the logic for clearing the content and rendering the page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import "./styles/footer.css";
 import "./styles/header.css";
-import "./styles/root.css"
+import "./styles/root.css";
+import createHomePage from "./pages/home.js";
+import createAboutPage from "./pages/about.js";
+import createMenuPage from "./pages/menu.js";
+import createContactPage from "./pages/contact.js";
+
+function clearContent() {
+    const content = document.querySelector('#content');
+    content.innerHTML = '';
+    content.className = '';
+}
+
+function initNavigation() {
+    const navButtons = document.querySelectorAll('.btn-link');
+    
+    navButtons[0].addEventListener('click', () => {
+        clearContent();
+        createHomePage();
+    });
+    
+    navButtons[1].addEventListener('click', () => {
+        clearContent();
+        createAboutPage();
+    });
+    
+    navButtons[2].addEventListener('click', () => {
+        clearContent();
+        createMenuPage();
+    });
+    
+    navButtons[3].addEventListener('click', () => {
+        clearContent();
+        createContactPage();
+    });
+}
+
+clearContent();
+createHomePage();
+initNavigation();
